037f0dc4b5c500d3f18f7b40c2cc34d0d72ef6cc
Sara-Alkhateeb <sarajamal11399@yahoo.com>
2023-03-06 12:04:18 +0300

    codeready

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS favMovies;
 CREATE TABLE IF NOT EXISTS favMovies(
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    releaseDate DATE,
+    releaseDate VARCHAR(255),
     posterPath VARCHAR(500),
     overview VARCHAR(10000)
 );
